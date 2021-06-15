75bba6f75f2d294848707e6b2e399710c8c01427
kidken285 <nguyenvinhtuan.2805@gmail.com>
2021-06-15 12:47:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import {NativeModules} from 'react-native';
 var RNExitApp = {
   exitApp: function() {
         NativeModules.RNExitApp.exitApp();
-  }
+  },
+  exitRestartApp: function() {
+      NativeModules.RNExitApp.exitRestartApp();
+  },
 };
 
 export default RNExitApp;
